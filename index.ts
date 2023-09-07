bdfa56ad0e4d1e235507190dbebdf1b18bff60ec
Sam <hangho.dev@gmail.com>
2023-09-07 22:16:09 +0700

    maths

---
+++ index.ts
@@ -0,0 +1,8 @@
+let chieudai = 4;
+let chieurong = 5;
+let a = "chu vi hinh chu nhat la:";
+let c = " dien tich hinh chu nhat:";
+let b = 2;
+
+console.log(a, (chieudai + chieurong) * b);
+console.log(c, chieudai * chieurong);
